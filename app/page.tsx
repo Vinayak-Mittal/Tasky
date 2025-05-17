fa4b820db3e17dead69a266edda28e64f10dd7a7
Vinayak Mittal <blissfulweb1@gmail.com>
2025-05-17 11:36:17 +0530

    yes

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TaskDashboard } from '@/components/task-dashboard';
-
-export default function Home() {
-  return (
-    <main className="container max-w-4xl mx-auto pt-16 pb-8 px-4">
-      <TaskDashboard />
-    </main>
-  );
-}
